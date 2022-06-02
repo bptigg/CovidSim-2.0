7710a2e6a21139f12da851aea3cb799727ac10c4
bptigg <64591476+bptigg@users.noreply.github.com>
2022-06-02 22:26:51 +0100

    tasks
    updates the task system be defining the task class

--- CovidSim/Source/task.cpp
+++ CovidSim/Source/task.cpp
@@ -1,1 +1,66 @@
 #include "task.h"
+
+std::string task::get_id()
+{
+    const void* m_address = (const void*)this;
+    std::stringstream ss;
+    ss << m_address;
+    return ss.str();
+}
+
+task::task()
+{
+    m_target_x = 0;
+    m_target_y = 0;
+    m_building_id = "";
+    m_target_agents = {};
+    m_task_length = 0;
+    m_current_run_time = 0; 
+
+    m_task_id = get_id();
+
+    task_initlized = false;
+}
+
+task::~task()
+{
+    m_target_agents.clear();
+}
+
+bool task::update_task()
+{
+    m_current_run_time++;
+
+    if (m_current_run_time >= m_task_length)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool task::initlize_task(int x, int y, std::string b_id, std::vector<std::shared_ptr<Agent>>& target_agents, int run_time)
+{
+    if (task_initlized == true)
+    {
+        std::string temp_message = "TASK" + m_task_id + "ALREADY INITLIZED, CHANGING TASK DEFINITION";
+        std::vector<char> chars(temp_message.begin(), temp_message.end());
+        chars.push_back('\0');
+        Log::info(&chars[0]);
+    }
+
+    m_target_x = x;
+    m_target_y = y;
+
+    m_building_id = b_id;
+    m_task_length = run_time;
+
+    for (int i = 0; i < target_agents.size(); i++)
+    {
+        m_target_agents.push_back(std::move(target_agents[i]));
+    }
+
+    task_initlized = true;
+}
--- CovidSim/Source/task.h
+++ CovidSim/Source/task.h
@@ -1,14 +1,43 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
+#include "Agent.h"
+#include "Log.h"
 
 class task
 {
 private:
+	std::string m_task_id;
+
 	int m_target_x;
 	int m_target_y;
+	std::string m_building_id;
 
-	std::string building_id
+	std::vector<std::shared_ptr<Agent>> m_target_agents;
+
+	int m_task_length;
+	int m_current_run_time;
+
+	bool task_initlized;
+
+public:
+	const std::string& task_id = m_task_id;
+	const std::string& building_id = m_building_id;
+
+	const int& task_length = m_task_length;
+	const int& current_run_time = m_current_run_time;
+
+private:
+	std::string get_id();
+
+public:
+	task();
+	~task();
+
+	bool update_task();
+	bool initlize_task(int x, int y, std::string b_id, std::vector<std::shared_ptr<Agent>>& target_agents, int run_time); //also allows redefinition of the task
+
 };
 
