dbf4706a3325fdd08d4aaef2888133d152a101ad
bptigg <64591476+bptigg@users.noreply.github.com>
2022-06-23 18:46:47 +0100

    friendship system v1.0
    untested but compiles

--- CovidSim/Source/friendship.cpp
+++ CovidSim/Source/friendship.cpp
@@ -9,8 +9,9 @@ void friendship::update_friendship_network()
 			std::string friend_id = m_agents[it->first]->friends[i];
 			if ((*m_friendship_network)(it->second, m_agents_index[friend_id]) == 0)
 			{
-				(*m_friendship_network)(it->second, m_agents_index[friend_id]) = 1;
-				(*m_friendship_network)(m_agents_index[friend_id], it->second) = 1;
+				double strength = get_bond_strength();
+				(*m_friendship_network)(it->second, m_agents_index[friend_id]) = strength;
+				(*m_friendship_network)(m_agents_index[friend_id], it->second) = strength;
 			}
 		}
 	}
@@ -18,25 +19,78 @@ void friendship::update_friendship_network()
 
 void friendship::identify_friendship_triangle()
 {
+	m_skip = true;
 	for (int i = 0; i < m_friendship_network->get_col_size(); i++)
 	{
-		for (int e = 0; e < m_friendship_network->get_row_size(); i++)
+		for (int e = 0; e < m_friendship_network->get_row_size(); e++)
 		{
-			if ((*m_friendship_network)(e, i) != 1);
+			if ((*m_friendship_network)(i, e) == 0);
 			{
-				for (int a = 0; a < m_agents[m_agents_index_ref[e]]->friends.size(); a++)
+				for (int a = 0; a < m_friendship_network->get_row_size(); a++)
 				{
-					std::string friend_agent = m_agents[m_agents_index_ref[e]]->friends[a];
-					if (friend_agent == m_agents_index_ref[i])
+					if ((*m_friendship_network)(e, a) != 0)
 					{
-						continue;
-					}
-					else
-					{
-						m_agents_triangles[m_agents_index_ref[i]].push_back(std::make_unique<triangle>(m_agents_index_ref[i], m_agents_index_ref[e], friend_agent));
+						if (i == a || triangles[a] == i || triangles[i] == a)
+						{
+							continue;
+						}
+						else
+						{
+							m_agents_triangles[std::make_pair(m_agents_index_ref[i],m_agents_index_ref[a])].push_back(std::make_shared<triangle>(m_agents_index_ref[i], m_agents_index_ref[e], m_agents_index_ref[a], (*m_friendship_network)(i, e), (*m_friendship_network)(e, a)));
+							triangles[i] = a;
+						}
 					}
 				}
 			}
 		}
 	}
+	get_confidence_value();
+	m_skip = false;
+}
+
+double friendship::get_bond_strength()
+{
+	return ((double)random::Random_number(0, 100) * 0.01);
+}
+
+void friendship::get_confidence_value()
+{
+	for (auto it = m_agents_triangles.begin(); it != m_agents_triangles.end(); it++)
+	{
+		int size = it->second.size();
+		double confidence_value = 0;
+		for (int i = 0; i < size; i++)
+		{
+			confidence_value = confidence_value + it->second[i]->p_strength;
+		}
+		m_triangle_confidence[it->first] = confidence_value;
+	}
+}
+
+std::vector<std::string> friendship::get_friends(std::pair<std::string, std::string> key)
+{
+	std::vector<std::string> friends;
+	std::vector<std::shared_ptr<triangle>> connections = m_agents_triangles[key];
+
+	for (int i = 0; i < connections.size(); i++)
+	{
+		friends.push_back(connections[i]->m_link_agent);
+	}
+	
+	return friends;
+}
+
+std::map<std::pair<std::string, std::string>, std::vector<std::string>> friendship::get_viable_pairing(double min_value)
+{
+	std::map<std::pair<std::string, std::string>, std::vector<std::string>> viable_pairing;
+	
+	for (auto it = m_triangle_confidence.begin(); it != m_triangle_confidence.end(); it++)
+	{
+		if (it->second >= min_value)
+		{
+			viable_pairing[it->first] = get_friends(it->first);
+		}
+	}
+
+	return viable_pairing;
 }
--- CovidSim/Source/friendship.h
+++ CovidSim/Source/friendship.h
@@ -2,6 +2,7 @@
 
 #include "Agent.h"
 #include "Matrix.h"
+#include "Random.h"
 
 #include <map>
 
@@ -14,9 +15,13 @@ private:
 		std::string m_link_agent;
 		std::string m_final_agent;
 
-		triangle(std::string m_t, std::string m_l, std::string m_f)
+		double p_strength;
+
+		triangle(std::string m_t, std::string m_l, std::string m_f, int tl, int lf)
 			:m_target_agent(m_t), m_link_agent(m_l), m_final_agent(m_f)
 		{
+			p_strength = (tl + lf) * 0.5;
+
 		}
 	};
 	
@@ -24,11 +29,24 @@ private:
 	std::map<int, std::string > m_agents_index_ref;
 	std::map<std::string, int> m_agents_index;
 
-	std::map<std::string, std::vector<std::unique_ptr<triangle>>> m_agents_triangles;
+	std::map<std::pair<std::string,std::string>, std::vector<std::shared_ptr<triangle>>> m_agents_triangles;
+	std::map<std::pair<std::string, std::string>, double> m_triangle_confidence;
+	std::map<int, int> triangles;
 
 	std::shared_ptr<Matrix<double>> m_friendship_network;
+
+	bool m_skip = false;
+public:
+	const bool& skip = m_skip;
 private:
 	void update_friendship_network();
 	void identify_friendship_triangle();
+	double get_bond_strength(); //will change at somepoint
+	void get_confidence_value();
+	std::vector<std::string> get_friends(std::pair<std::string, std::string> key);
+
+public:
+	std::map<std::pair<std::string, std::string>, std::vector<std::string>> get_viable_pairing(double min_value);
+
 };
 
